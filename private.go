018397d1822fd736851ae38fc2719e799eda8e7b
paul <paul@cueb.io>
2018-10-13 16:33:19 -0700

    hacky name fix

--- private.go
+++ private.go
@@ -6,7 +6,7 @@ import (
 )
 
 // checks the validity of a name according to the spec: http://facebook.github.io/graphql/October2016/#sec-Names
-var validName = regexp.MustCompile("^[_A-Za-z][_0-9A-Za-z]*$")
+var validName = regexp.MustCompile("^[_A-Za-z][@_0-9A-Za-z]*$")
 
 func isValidOperationType(Type operationType) bool {
 	low := strings.ToLower(string(Type))
